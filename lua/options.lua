6dcf0ead5314f1ffca2c70cf71e1709aa69f7a13
Mike Mellenthin <mike.mellenthin@gmail.com>
2025-05-05 09:39:40 -0400

    set tab width to 2 for everything but go

--- lua/options.lua
+++ lua/options.lua
@@ -5,10 +5,15 @@ vim.g.loaded_netrwPlugin = 1
 vim.opt.ruler = true
 vim.opt.nu = true
 vim.opt.mouse = "a"
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.fillchars = { eob = " " }
 
 vim.keymap.set("i", "kj", "<Esc>")
 
 vim.diagnostic.config({ virtual_text = true })
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "go",
+	command = "setlocal tabstop=4 shiftwidth=4",
+})
