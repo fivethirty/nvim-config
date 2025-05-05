6fa67e91eefe3c5790206c1f4ddf904da43bceb6
Mike Mellenthin <mike.mellenthin@gmail.com>
2025-05-05 09:28:24 -0400

    update some nvim-tree options

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -11,6 +11,19 @@ return {
 				enable = true,
 				show_on_dirs = true,
 			},
+			view = {
+				signcolumn = "no",
+			},
+			renderer = {
+				highlight_diagnostics = "name",
+				highlight_hidden = "name",
+				icons = {
+					diagnostics_placement = "after",
+				},
+			},
+			update_focused_file = {
+				enable = true,
+			},
 		})
 		vim.keymap.set("n", "<leader>ff", ":NvimTreeToggle<CR>", { desc = "NvimTree toggle filesystem" })
 		if vim.fn.argc(-1) == 0 then
