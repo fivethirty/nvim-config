fc8cd04427e9d5e0ff8ff5553463b12068b05bb1
Mike Mellenthin <mike.mellenthin@gmail.com>
2025-05-05 23:19:30 -0400

    only add lsp kepmaps when lsp is attached

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -2,3 +2,15 @@ vim.api.nvim_create_autocmd("FileType", {
 	pattern = "go",
 	command = "setlocal tabstop=4 shiftwidth=4",
 })
+
+vim.api.nvim_create_autocmd("LspAttach", {
+	desc = "LSP Actins",
+	callback = function(event)
+		local opts = {
+			buffer = event.buf,
+		}
+		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+		vim.keymap.set("n", "gd", vim.lsp.buf.implementation, opts)
+		vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+	end,
+})
